02ac06b395da991ff845d57bd0e63b4cfff44869
kengirie <kengoi.6330@gmail.com>
2026-08-11 01:17:34 -0700

    Drop bundled web fonts and render with system fonts

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,6 @@ import App from './App.tsx';
 import './index.css';
 import './i18n';
 
-// Self-hosted fonts (CSP restricts font-src to 'self')
-import '@fontsource/shippori-mincho-b1/600.css';
-import '@fontsource/shippori-mincho-b1/700.css';
-import '@fontsource/ibm-plex-sans-jp/400.css';
-import '@fontsource/ibm-plex-sans-jp/500.css';
-import '@fontsource/ibm-plex-sans-jp/700.css';
-import '@fontsource/ibm-plex-mono/400.css';
-import '@fontsource/ibm-plex-mono/500.css';
-
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
     <App />
